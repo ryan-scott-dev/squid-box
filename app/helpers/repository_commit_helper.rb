902bcbe4e9935ba4029f8152b4b3ce9d185e2789
Ryan Scott <ryan.scott@lambdasoftware.com.au>
2012-07-05 21:45:26 +1000

    Implemented a diff that only shows the changes to a file and not the whole file

--- app/helpers/repository_commit_helper.rb
+++ app/helpers/repository_commit_helper.rb
@@ -5,8 +5,37 @@ module RepositoryCommitHelper
     line.sub(/^./, '').chomp
   end
 
-  def produce_full_diff(diff)
+  def produce_full_unified_diff(diff)
     produce_diff diff
+  end
+
+  def produce_context_unified_diff(diff)
+    produce_diff diff, {:diff => %W(-U 10), :include_diff_info => true}
+  end
+
+  def find_line_type(line)
+    case line
+      when /^(---|\+\+\+|\\\\)/
+        :comment
+      when /^\+/
+        :added
+      when /^-/
+        :deleted
+      when /^ /
+        :unchanged
+      when /^@@/
+        :block_info
+      else
+        :unknown
+    end
+  end
+
+  def before_start_line(line)
+    line.match(/@@ -(\d+).*\+(\d+),.*/)[1].to_i
+  end
+
+  def after_start_line(line)
+    line.match(/@@ -(\d+).*\+(\d+),.*/)[2].to_i
   end
 
   private
