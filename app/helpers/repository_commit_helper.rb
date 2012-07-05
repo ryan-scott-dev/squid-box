2091cb5223b6d316cce548d4eb65091777cd2f6f
Ryan Scott <ryan.scott@lambdasoftware.com.au>
2012-07-05 20:00:01 +1000

    Refactored the method to generate a diff to prepare for other ways to view the diff

--- app/helpers/repository_commit_helper.rb
+++ app/helpers/repository_commit_helper.rb
@@ -5,4 +5,15 @@ module RepositoryCommitHelper
     line.sub(/^./, '').chomp
   end
 
+  def produce_full_diff(diff)
+    produce_diff diff
+  end
+
+  private
+  def produce_diff(diff, options = {})
+    original = diff.a_blob.nil? ? nil : diff.a_blob.data
+
+    Diffy::Diff.new original, diff.b_blob.data, {:allow_empty_diff => true}.merge(options)
+  end
+
 end
